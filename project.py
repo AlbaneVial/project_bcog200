3bd5a3d2be75168e4849e18c0baa9a01ee4aebbe
AlbaneVial <avial2@illinois.edu>
2025-03-06 16:50:20 -0600

    begin code

---
+++ project.py
@@ -0,0 +1,99 @@
+import tkinter as tk
+
+
+class player:
+    def __init__(self, personality, color, score=300):
+        self.personality = personality
+        self.color = color
+        self.score = score
+
+    def play(self, history):
+        if self.personality == "cheater":
+            return "cheat"
+        if self.personality == "gudger":
+            if "cheat" in history:
+                return "cheat"
+            else:
+                return "cooperate"
+        if self.personality == "tit_for_tat":
+            if "cheat" in history[:-1]:
+                print(history[:-1])
+                return "cheat"
+            else:
+                return "cooperate"
+        if self.personality == "cooperator":
+            return "cooperate"
+
+
+def play_round(player1, player2, nb_match):
+    history1 = []
+    history2 = []
+
+    for i in range(nb_match):
+        strategy1 = player1.play(history1)
+        strategy2 = player2.play(history2)
+        history1.append(strategy2)
+        history2.append(strategy1)
+        if strategy1 == "cooperate" and strategy2 == "cooperate":
+            player1.score += 3
+            player2.score += 3
+        elif strategy1 == "cooperate" and strategy2 == "cheat":
+            player1.score -= 3
+            player2.score += 6
+        elif strategy1 == "cheat" and strategy2 == "cooperate":
+            player1.score += 6
+            player2.score -= 3
+
+
+class canvas:
+    def __init__(self, nb_tit_for_tat=4, nb_cheater=4, nb_gudger=4, nb_cooporate=4):
+        """ """
+        pass
+
+
+def verification(nb_tit_for_tat, nb_cheater, nb_gudger, nb_cooporate, nb_match):
+    return (
+        int(nb_tit_for_tat),
+        int(nb_cheater),
+        int(nb_gudger),
+        int(nb_cooporate),
+        int(nb_match),
+    )
+
+
+def main():
+    print("empty!")
+    nb_tit_for_tat = input(f"nb_tit_for_tat: ")
+    nb_cheater = input(f"nb_cheater: ")
+    nb_gudger = input(f"nb_gudger: ")
+    nb_cooperator = input(f"nb_cooperator: ")
+    nb_match = input("nb_match: ")
+    nb_tit_for_tat, nb_cheater, nb_gudger, nb_cooperator, nb_match = verification(
+        nb_tit_for_tat, nb_cheater, nb_gudger, nb_cooperator, nb_match
+    )
+    list_tit_for_tat = [player("tit_for_tat", "red") for _ in range(nb_tit_for_tat)]
+    list_cheater = [player("cheater", "blue") for _ in range(nb_cheater)]
+    list_gudger = [player("gudger", "green") for _ in range(nb_gudger)]
+    list_cooperator = [player("cooperator", "yellow") for _ in range(nb_cooperator)]
+    total_player = nb_tit_for_tat + nb_cooperator + nb_gudger + nb_cheater
+    all_players = list_tit_for_tat + list_cheater + list_gudger + list_cooperator
+
+    while (
+        nb_tit_for_tat != total_player
+        or nb_cheater != total_player
+        or nb_gudger != total_player
+        or nb_cooperator != total_player
+    ):
+        for i in range(len(all_players)):
+            for j in range(i + 1, len(all_players)):
+                play_round(all_players[i], all_players[j], nb_match)
+            print(
+                f"score of players {all_players[i].personality}: {all_players[i].score } "
+            )
+        continue_playing = input("Do you want to continue? (yes/no): ").strip().lower()
+        if continue_playing != "yes":
+            break
+
+
+if __name__ == "__main__":
+    main()
